d5d148e4a7e7e4ed36cb56f3497aee91b0305f4e
medvol <95881208+medvol@users.noreply.github.com>
2022-05-15 11:55:17 +0000

    Deploying to gh-pages from @ medvol/team-project-5@ca46cd89d2e1c6fa9eceae00ff6016cc3b663848 🚀

--- src.7ad70d79.js
+++ src.7ad70d79.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/team-project-5/src.93a041a7.js.map
+//# sourceMappingURL=/team-project-5/src.7ad70d79.js.map
